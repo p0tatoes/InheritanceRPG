26041dea555a08ab18a0e7a2eb83acf77629a886
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2021-04-02 16:37:24 +0800

    Added retry button on end screen

--- app/src/main/java/com/example/inheritancerpg/EndScreen.java
+++ app/src/main/java/com/example/inheritancerpg/EndScreen.java
@@ -2,27 +2,48 @@ package com.example.inheritancerpg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class EndScreen extends AppCompatActivity {
+public class EndScreen extends AppCompatActivity implements View.OnClickListener {
 
     TextView endTxt;
+    Button retryBtn;
+    boolean victory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
 
+        // Declaration for UI elements
+        retryBtn = findViewById(R.id.retryBtn);
+        retryBtn.setOnClickListener(this);
         endTxt = findViewById(R.id.endTxt);
 
-        boolean victory = getIntent().getBooleanExtra("VICTORY", true);
+        // Gets victory value from previous activities
+        victory = getIntent().getBooleanExtra("VICTORY", true);
 
         if (victory) {
             endTxt.setText("Salutations, hero! You have vanquished your foes.");
         }
         else {
             endTxt.setText("You have been vanquished");
+        }
+    }
+
+    @Override
+    public void onClick (View v) {
+
+        Intent start = new Intent(this, SelectionScreen.class);
+
+        switch (v.getId()) {
+            case R.id.retryBtn:
+                startActivity(start);
+            break;
         }
     }
 }
